d6ede8ae5bd48fb8c1ac266a078378a5bb758e69
Matheus Avilla Vial <matheus.vial@facens.br>
2022-05-04 23:08:51 -0300

    [ADD]: Projeto TechTherapy

---
+++ src/classes/PwdPlaceholder.java
@@ -0,0 +1,17 @@
+
+package classes;
+
+import java.awt.Color;
+import java.awt.Font;
+import javax.swing.JTextField;
+
+
+public class PwdPlaceholder extends javax.swing.JFrame {
+
+    public PwdPlaceholder() {
+        
+    }
+    
+    
+    
+}
